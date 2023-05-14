750f769c2c67713e3358727d9322373e69fe039c
Александр Фадеев <dj-soft@mail.ru>
2023-05-14 14:18:00 +0300

    Устранены все замечания ревьюера

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'ENGINE': os.getenv('DB_ENGINE'),
         'NAME': os.getenv('DB_NAME', default='default'),
         'USER': os.getenv('POSTGRES_USER', default='default'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='default'),
